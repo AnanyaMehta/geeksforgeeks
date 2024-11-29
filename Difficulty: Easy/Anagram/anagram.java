8ed7d5a70e144b63df5aa1b8998d3fcbd6cce482
Ananya Mehta <78225588+AnanyaMehta@users.noreply.github.com>
2024-11-30 00:23:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+            int n = s1.length();
+            int freq1[]=new int[26], freq2[]=new int[26];
+            if(s1.length() != s2.length()) return false;
+            int j=0;
+            for(int i=0;i<n;i++){
+                j=(int)(s1.charAt(i))-97;
+                freq1[j]++;
+                
+                j=(int)(s2.charAt(i))-97;
+                freq2[j]++;
+            }
+            for(int i=0;i<26;i++){
+                if(freq1[i]!=freq2[i]) return false;
+            }
+            return true;
+    }
+}
+
